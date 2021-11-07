27582f6160a7e7367b400f53fe4ec30612b92744
Denilson Silva <denilson.bslv@gmail.com>
2021-11-07 01:04:01 -0300

    Atualização gitignore

--- database/database.js
+++ database/database.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 
-const connection = new Sequelize('bd', 'user', 'pass',{
-    host: 'host',
-    dialect: 'bd-lanquage'
+const connection = new Sequelize('bdProjetos', 'admin', 'd9z1199N',{
+    host: 'mysql-projetos.cjhwk0qrane0.us-east-2.rds.amazonaws.com',
+    dialect: 'mysql'
 })
 
 module.exports = connection;
